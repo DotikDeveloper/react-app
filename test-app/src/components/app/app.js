8821d4265c38d8629e037b1c064cb9266c81fad6
Dotik <36793535+DotikDeveloper@users.noreply.github.com>
2020-12-21 13:05:44 +0400

    add onToggleImportant and onToggleLike

--- test-app/src/components/app/app.js
+++ test-app/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
@@ -6,19 +6,109 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
+import styled from 'styled-components';
 
-const App = () => {
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {label: 'Going to learn React.js', important: true, like: false, id: 1},
+                {label: 'Going to learn Golang', important: false, like: false, id: 2},
+                {label: 'Going to learn Node.js', important: false, like: false, id: 3}
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+        this.onToggleImportant = this.onToggleImportant.bind(this);
+        this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.maxId = 4;
+    }
+
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);   
+            const newArray = [...data.slice(0, index), ...data.slice(index + 1)];
+
+            return {
+                data: newArray
+            }
+        })
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArray = [...data, newItem];
+            return {
+                data: newArray
+            }
+        })
+    }
+
+    onToggleImportant(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, important: !old.important};
+            const newArray = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {data: newArray}
+
+        })
+    }
+
+    onToggleLiked(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, like: !old.like};
+            const newArray = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {data: newArray}
+
+        })
+    }
+
+    render() {
+    const {data} = this.state;     
+
+    const liked = data.filter(item => item.like).length;
+    const allPosts = data.length;
+
     return (
-        <div className="app">
-            <AppHeader/>
+        <AppBlock>
+            <AppHeader
+                liked={liked}
+                allPosts={allPosts}
+            />
             <div className="search-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
-            <PostAddForm/>
-        </div>            
-    )
+            <PostList 
+                posts={this.state.data}
+                onDelete={this.deleteItem}
+                onToggleImportant={this.onToggleImportant}
+                onToggleLiked={this.onToggleLiked}
+                />
+            <PostAddForm
+                onAdd={this.addItem}/>
+        </AppBlock>            
+    )            
+}
 }
 
-export default App;
+
--- test-app/src/components/post-list-item/post-list-item.js
+++ test-app/src/components/post-list-item/post-list-item.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './post-list-item.css';
 
-const PostListItem = () => {
-    return (
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Hello World!
-            </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button type="button" className="btn-star btn-sm">
-                    <i className="fa fa-star"></i>
-                </button>
-                <button type="button" className="btn-trash btn-sm">
-                    <i className="far fa-trash-alt"></i>
-                </button>
-                <i className="far fa-heart"></i>
+export default class PostListItem extends Component {
+
+
+
+    render () {
+
+        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
+        let classNames = 'app-list-item d-flex justify-content-between';
+
+        if (important) {
+            classNames += ' important';
+        } 
+        if (like) {
+            classNames += ' like';
+        } 
+
+        return (
+            <div className={classNames}>
+                <span 
+                    className="app-list-item-label"
+                    onClick={onToggleLiked}>
+                    {label}
+                </span>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button 
+                        type="button" 
+                        className="btn-star btn-sm"
+                        onClick={onToggleImportant}>
+                            <i className="fa fa-star"></i>
+                    </button>
+                    <button 
+                        type="button" 
+                        className="btn-trash btn-sm"
+                        onClick={onDelete}>
+                            <i className="far fa-trash-alt"></i>
+                    </button>
+                    <i className="fas fa-heart"></i>
+                </div>
             </div>
-        </li>
-
-    )
-}
-
-export default PostListItem;
+        )
+    }
+} 
